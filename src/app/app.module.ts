c54d0a0a16ba9ffb952d3d3b1cf5a134c596a7ca
erauture <pandamy.yt@gmail.com>
2024-07-29 08:02:00 +0200

    feat(2) update navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { QuestionComponent } from './question/question.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarLeftComponent } from './navbar/navbar-left/navbar-left.component';
+import { NavbarCenterComponent } from './navbar/navbar-center/navbar-center.component';
+import { NavbarRightComponent } from './navbar/navbar-right/navbar-right.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
     QuestionComponent,
-    NavbarComponent
+    NavbarLeftComponent,
+    NavbarCenterComponent,
+    NavbarRightComponent
   ],
   imports: [
     BrowserModule,
